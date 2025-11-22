5b62205eecc69edec37f04b5189d524bdf8cdb13
eslam985 <ee17172@gmail.com>
2025-11-22 18:40:59 +0200

    حذف علامة الدولار في سعر المنتج

--- src/App.jsx
+++ src/App.jsx
@@ -96,25 +96,6 @@ const SendInputIcon = (props) => (
   </svg>
 );
 
-const DollarIcon = (props) => (
-  <svg
-    {...props}
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <line x1="12" x2="12" y1="2" y2="22" />
-    <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-    <title>أيقونة سعر</title>
-  </svg>
-);
-
 const WhatsAppSendIcon = (props) => (
   <svg
     {...props}
